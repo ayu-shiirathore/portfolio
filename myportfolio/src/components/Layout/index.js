24cfc9fbb2a54a7202c1cbc66536288fdc386c8d
ayu-shiirathore <ayurathore2307@gmail.com>
2024-05-31 01:10:12 +0530

    made sidebar

---
+++ myportfolio/src/components/Layout/index.js
@@ -0,0 +1,14 @@
+
+import Sidebar from '../Sidebar'
+import './index.scss'
+
+const Layout =() => {
+    return(
+        <div className='App'>
+        <Sidebar />
+        <div className='page'></div>
+        </div>
+    ) 
+}
+
+export default Layout
