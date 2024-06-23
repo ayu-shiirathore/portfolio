78271250c4f139768633d3d88adbb8c29baa5b62
ayu-shiirathore <ayurathore2307@gmail.com>
2024-06-23 12:31:49 +0530

    resume updated

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -49,7 +49,7 @@ const Home = () => {
                 <h2>Web Develoment / Frontend Developer / DataScience</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
                 {/* <Link to="/resume" className='flat-button'>RESUME</Link> */}
-                <a href="https://drive.google.com/file/d/1_UO5KGZe90Spg-EZzIjl8yCeECrGf_0T/view?usp=drive_link" className='flat-button' target="_blank" rel="noopener noreferrer">RESUME</a>
+                <a href="https://drive.google.com/file/d/1atWcfdaVSMV_4SFRo2QW5AtMZ4yl-EnA/view?usp=drive_link" className='flat-button' target="_blank" rel="noopener noreferrer">RESUME</a>
             </div>
             <Logo />
         </div>
